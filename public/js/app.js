75a67c8d9186cec124306010d756806c1161207d
Emilia Rodriguez Gimenez <emiliamartin97@gmail.com>
2020-04-09 15:45:37 -0300

    Public folder with styles, images and js file

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+console.log('Client side javascript file is loaded')
+
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+  response.json().then((data) => {
+    console.log(data);
+  })
+})
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-one');
+const messageTwo = document.querySelector('#message-two');
+
+
+weatherForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const location = search.value;
+  messageOne.textContent = 'Searching...';
+  messageTwo.textContent = '';
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageTwo.textContent = '';
+        messageOne.textContent = 'There has been an error';
+      } else {
+        messageOne.textContent = location;
+        messageTwo.textContent = `It is currently ${data.temp} in ${location}. There is a ${data.rainProb}% chance of rain`;
+      }
+    })
+  })
+})
